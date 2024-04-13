25b4436b65c962a44e489b60a2880cac463585c0
CTY <polaris150566@mail.ustc.edu.cn>
2024-04-13 15:30:07 +0800

    4131529

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+//const { create, t, inform, exec, bundle, setParser} = require('ef.js');
+import navigation_formation from './navigation_formation.eft'
+import './index.less'
+const navigation = new navigation_formation();
+navigation.$mount({target:document.body,option:"append"});
 const a = 0;
